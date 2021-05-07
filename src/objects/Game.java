ac57a7d7b9b0a0694d905e1f8c001283d2f2cc64
Aden <parsosna1011@gmail.com>
2021-05-07 08:54:16 -0400

    Edited game

--- src/objects/Game.java
+++ src/objects/Game.java
@@ -1,7 +1,39 @@
 package objects;
 
+import java.util.HashMap;
+
+import workers.WorkerClass;
+
 public class Game {
 
+	private int cadens;	
+		
+	private WorkerClass chrisworker;
+	private WorkerClass hunterworker;
+	private WorkerClass sirevrimworker;
+	private WorkerClass tannerworker;
+	private WorkerClass lostdollaworker;
+
 	
+	public Game() {
+		this.cadens = 0;
+		this.chrisworker = new WorkerClass(1);
+		this.hunterworker = new WorkerClass(2);
+		this.sirevrimworker = new WorkerClass(3);
+		this.tannerworker = new WorkerClass(4);
+		this.lostdollaworker = new WorkerClass(5);
+	}
+	
+	public void addCadens(int cadens) {
+		this.cadens += cadens;
+	}
+	
+	public void setCadens(int cadens) {
+		this.cadens = cadens;
+	}
+	
+	public void removeCadens(int cadens) {
+		this.cadens -= cadens;
+	}
 	
 }
