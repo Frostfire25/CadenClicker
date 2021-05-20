39a791764f1cf328eaed1a92f674a91932697b48
Elguezabal - Alexander Theodore <ElguezabalAT21@londonderry.org>
2021-05-20 08:38:44 -0400

    fixed gui errors

--- src/graphics/CadenClickerGUI.java
+++ src/graphics/CadenClickerGUI.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class CadenClickerGUI extends JFrame {
 
@@ -14,29 +17,29 @@ public class CadenClickerGUI extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CadenClickerGUI frame = new CadenClickerGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
+	
+	public void displayGUI() {
+		setVisible(true);
+	}
+	
+	public void hideGUI() {
+		setVisible(false);
+	}
+	
 	public CadenClickerGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout());
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(103, 99, 46, 14);
+		contentPane.add(lblNewLabel);
 	}
-
 }
